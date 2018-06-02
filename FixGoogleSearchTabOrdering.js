29a44af40819c22b6a470287ad00dd103161f71a
Alex Doub <5578462+alexdoub@users.noreply.github.com>
2018-06-02 15:27:02 -0700

    Add files via upload

---
+++ FixGoogleSearchTabOrdering.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name        Google Fixed Tab Order
+// @namespace   google.com
+// @include     https://www.google.com/search?*
+// @include     https://www.google.com/webhp?*
+// @include     https://www.google.ca/search?*
+// @include     https://www.google.ca/webhp?*
+// @version     2.0
+// @grant       none
+// ==/UserScript==
+
+(function ()
+{
+        var order = ["All", "Web", "Images", "Videos", "News", "Maps", "Books", "Apps", "Shopping", "Flights"];
+
+        function observerEnable()
+        {
+                observer.observe(document.querySelector("#main"), { childList: true, subtree: true });
+        }
+        function observerDisable()
+        {
+                observer.disconnect();
+        }
+        var observer = new MutationObserver(function(mutations)
+        {
+                observerDisable();
+                fixTabs();
+                observerEnable();
+        });
+        observerEnable();
+
+        function fixTabs()
+        {
+                var parent = document.querySelector("#hdtb-msb-vis");
+                if (parent == null) {
+                        return;
+                }
+
+                var all = parent.querySelectorAll(".hdtb-mitem.hdtb-msel.hdtb-imb");
+                var tabs = parent.querySelectorAll(".hdtb-mitem");
+
+                var more = document.querySelector("#hdtb-more");
+                var tools = document.querySelector("#hdtb-tls");
+
+                while (parent.firstChild) {
+                        parent.removeChild(parent.firstChild);
+                }
+
+                for (var i = 0; i < order.length; i++) {
+                        for (var t = 0; t < tabs.length; t++) {
+                                if (order[i] == tabs[t].textContent) {
+                                        parent.appendChild(tabs[t]);
+                                }
+                        }
+                }
+                parent.appendChild(tools);
+        }
+        fixTabs();
+})();
